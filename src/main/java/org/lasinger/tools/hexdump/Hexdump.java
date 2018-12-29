c39431116c0d4f567195ef6e2faeea83c149f747
Florian Lasinger <florian@lasinger.org>
2018-12-29 10:13:00 +0100

    add implementation

---
+++ src/main/java/org/lasinger/tools/hexdump/Hexdump.java
@@ -0,0 +1,108 @@
+package org.lasinger.tools.hexdump;
+
+public class Hexdump {
+
+    private static final char[] HEX =
+            new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', };
+
+    private Hexdump() {
+    }
+
+    public static String hexdump(byte[] bytes) {
+        if (bytes == null) {
+            throw new IllegalArgumentException("byte array may not be null");
+        }
+        if (bytes.length == 0) {
+            return "";
+        }
+
+        StringBuilder out = new StringBuilder();
+
+        for (int offset = 0; offset < bytes.length; offset += 16) {
+            appendLine(bytes, offset, out);
+        }
+        appendOffset(bytes.length, out);
+        out.append('\n');
+
+        return out.toString();
+    }
+
+    private static void appendLine(byte[] bytes, int firstBlockStart, StringBuilder out) {
+        int firstBlockEnd = Math.min(bytes.length, firstBlockStart + 8);
+        int secondBlockEnd = Math.min(bytes.length, firstBlockStart + 16);
+
+        appendOffset(firstBlockStart, out);
+        out.append(' ');
+        out.append(' ');
+
+        appendHexBytes(bytes, firstBlockStart, firstBlockEnd, out);
+        out.append(' ');
+
+        appendHexBytes(bytes, firstBlockStart + 8, secondBlockEnd, out);
+        padMissingBytes(firstBlockStart, secondBlockEnd, out);
+        out.append(' ');
+
+        out.append('|');
+        appendDisplayChars(bytes, firstBlockStart, secondBlockEnd, out);
+        out.append('|');
+        out.append('\n');
+    }
+
+    private static void appendOffset(int offset, StringBuilder out) {
+        int x = offset;
+        for (int i = 0; i < 4; i++) {
+            x = Integer.rotateLeft(x, 8);
+            Hexdump.appendHexChars((byte) (x & 0xFF), out);
+        }
+    }
+
+    private static void appendHexBytes(byte[] bytes, int offset, int limit, StringBuilder out) {
+        for (int i = offset; i < limit; i++) {
+            appendHexChars(bytes[i], out);
+            out.append(' ');
+        }
+    }
+
+    private static void appendHexChars(byte b, StringBuilder out) {
+        out.append(HEX[(b >> 4) & 0x0F]); // 4 high bits
+        out.append(HEX[b & 0x0F]); // 4 low bits
+    }
+
+    private static void padMissingBytes(int firstByte, int lastByte, StringBuilder out) {
+        int charsPerByte = 3;
+        int maxBytesPerLine = 16;
+        int bytesWritten = lastByte - firstByte;
+
+        int charsMissing = charsPerByte * (maxBytesPerLine - bytesWritten);
+        for (int i = 0; i < charsMissing; i++) {
+            out.append(' ');
+        }
+    }
+
+    private static void appendDisplayChars(byte[] bytes, int offset, int blockEnd, StringBuilder out) {
+        for (int i = offset; i < blockEnd; i++) {
+            appendDisplayChar(bytes[i], out);
+        }
+    }
+
+    private static void appendDisplayChar(byte b, StringBuilder out) {
+        switch (b) {
+        case 0x20:
+            out.append("\u2423"); // SPACE ␣
+            break;
+        case 0x09:
+            out.append('\u2192'); // TAB →
+            break;
+        case 0x0a:
+            out.append('\u00b6'); // LF ¶
+            break;
+        case 0x0d:
+            out.append('\u00a4'); // CR ¤
+            break;
+        default:
+            out.append((32 <= b && b <= 126) ? (char) b : '\u00b7'); // ' ' to '~', non-printable is MEDIUM DOT ·
+        }
+
+    }
+
+}
---
+++ src/test/java/org/lasinger/tools/hexdump/HexdumpTest.java
@@ -0,0 +1,125 @@
+package org.lasinger.tools.hexdump;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.lasinger.tools.hexdump.Hexdump.hexdump;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class HexdumpTest {
+
+    @Test
+    public void example_gitignore() throws Exception {
+        String in = "/.classpath\n" + //
+                "/.project\n" + //
+                "/.settings/\n" + //
+                "/target/\n" + //
+                "/nbproject/\n" + //
+                "/nb-configuration.xml";
+        System.out.println(hexdump(in.getBytes(UTF_8)));
+    }
+
+    @Test
+    public void empty() throws Exception {
+        assertThat(hexdump(new byte[0])).isEqualTo("");
+    }
+
+    @Test
+    public void hexEncoding() throws Exception {
+        String in = "1fe20bc4";
+        int i = Integer.parseInt(in, 16);
+        byte[] bytes = new byte[] { (byte) (i >>> 24), (byte) (i >>> 16), (byte) (i >>> 8), (byte) i };
+
+        String dump = hexdump(bytes);
+
+        System.out.println(dump);
+        assertThat(dump.replaceAll(" ", "")).contains(in);
+    }
+
+    @Test
+    public void eightBytes() throws Exception {
+        String dump = hexdump(new byte[] { 48, 49, 50, 51, 52, 53, 54, 55 });
+
+        System.out.println(dump);
+        assertThat(dump).startsWith("00000000  30 31 32 33 34 35 36 37                           |01234567|");
+    }
+
+    @Test
+    public void nineBytes() throws Exception {
+        String dump = hexdump(new byte[] { 48, 49, 50, 51, 52, 53, 54, 55, 56 });
+
+        System.out.println(dump);
+        assertThat(dump).startsWith("00000000  30 31 32 33 34 35 36 37  38                       |012345678|");
+    }
+
+    @Test
+    public void specialBytes() throws Exception {
+        String dump = hexdump(new byte[] { 0x09, 0x20, 0x20, 0x0D, 0x0A, 0x2E, 0x00 });
+
+        System.out.println(dump);
+        assertThat(dump).startsWith("00000000  09 20 20 0d 0a 2e 00                              |→␣␣¤¶.·|");
+    }
+
+    @Test
+    public void edgeBytes() throws Exception {
+        String dump = hexdump(new byte[] { 0x00, Byte.MIN_VALUE, Byte.MAX_VALUE, (byte) 255 });
+
+        System.out.println(dump);
+        assertThat(dump).startsWith("00000000  00 80 7f ff                                       |····|");
+    }
+
+    @Test
+    public void singleLine() throws Exception {
+        String dump = hexdump("1234".getBytes(UTF_8));
+        assertThat(dump).isEqualTo("00000000  31 32 33 34                                       |1234|\n" //
+                + "00000004\n");
+    }
+
+    @Test
+    public void singleLineFull() throws Exception {
+        String dump = hexdump("1234567812345678".getBytes(UTF_8));
+        assertThat(dump).isEqualTo("00000000  31 32 33 34 35 36 37 38  31 32 33 34 35 36 37 38  |1234567812345678|\n" //
+                + "00000010\n");
+    }
+
+    @Test
+    public void multiLine() throws Exception {
+        String dump = hexdump("1234567812345678abc".getBytes(UTF_8));
+        assertThat(dump).isEqualTo("00000000  31 32 33 34 35 36 37 38  31 32 33 34 35 36 37 38  |1234567812345678|\n"
+                + "00000010  61 62 63                                          |abc|\n" //
+                + "00000013\n");
+    }
+
+    @Test
+    public void printableChars() throws Exception {
+        String nonAscii = "äöüß";
+
+        String in = " !\"#$%&'()*+,-./0123456789:;<=>?@ABC-" + nonAscii + "-XYZ[\\]^_`abc-xyz{|}~";
+
+        String dump = hexdump(in.getBytes(UTF_8));
+
+        System.out.println(dump);
+        String printableAscii = in.replace(nonAscii, "");
+        assertThat(codepointList(dump)).containsAll(codepointList(printableAscii));
+        assertThat(codepointList(dump)).doesNotContainAnyElementsOf(codepointList(nonAscii));
+        assertThat(dump).contains("ABC-········-X");
+    }
+
+    // this test currently has no assertion. it is for visual output only.
+    @Test
+    public void fullByteRange() throws Exception {
+        byte[] allBytes = new byte[256];
+        for (int i = 0; i < 256; i++) {
+            allBytes[i] = (byte) i;
+        }
+        System.out.println(hexdump(allBytes));
+    }
+
+    private List<Integer> codepointList(String string) {
+        return string.codePoints().collect(ArrayList::new, List::add, List::addAll);
+    }
+
+}
